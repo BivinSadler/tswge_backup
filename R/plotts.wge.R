99a036ed446ab2da02694748e89d06295a4a2245
Sadler, Bivin Philip <bsadler@mail.smu.edu>
2021-02-03 00:06:33 -0600

    sdfsdfsd

--- R/plotts.wge.R
+++ R/plotts.wge.R
@@ -1,4 +1,4 @@
-plotts.wge = function (x, yLabel = "", Presentation = 0, title = "Realization", xLabel = "Time") 
+plotts.wge = function (x,Presentation = 0,ylab = "",xlab = "Time", title = "Realization") 
 {
   cex.labs <- c(0.9, 0.8, 0.9)
   numrows <- 1
@@ -10,25 +10,25 @@ plotts.wge = function (x, yLabel = "", Presentation = 0, title = "Realization", 
     if (class(x) != "ts") {
       if (n <= 200) {
         plot(t, x, type = "o", cex = 0.5, pch = 16, cex.lab = 0.75, 
-             cex.axis = 0.75, lwd = 0.75, xlab = xLabel, 
-             ylab = yLabel, col = "blue", main = title)
+             cex.axis = 0.75, lwd = 0.75, xlab = xlab, 
+             ylab = ylab, col = "blue", main = title)
       }
       else if (n > 200) {
         plot(t, x, type = "l", cex = 0.5, pch = 16, cex.lab = 0.75, 
-             cex.axis = 0.75, lwd = 0.75, xlab = xLabel, 
-             ylab = yLabel, col = "blue", main = title)
+             cex.axis = 0.75, lwd = 0.75, xlab = xlab, 
+             ylab = ylab, col = "blue", main = title)
       }
     }
     if (class(x) == "ts") {
       if (n <= 200) {
         plot(x, type = "o", cex = 0.5, pch = 16, cex.lab = 0.75, 
-             cex.axis = 0.75, lwd = 0.75, xlab = xLabel, 
-             ylab = yLabel, col = "blue", main = title)
+             cex.axis = 0.75, lwd = 0.75, xlab = xlab, 
+             ylab = ylab, col = "blue", main = title)
       }
       else if (n > 200) {
         plot(x, type = "l", cex = 0.5, pch = 16, cex.lab = 0.75, 
-             cex.axis = 0.75, lwd = 0.75, xlab = xLabel, 
-             ylab = yLabel, col = "blue", main = title)
+             cex.axis = 0.75, lwd = 0.75, xlab = xlab, 
+             ylab = ylab, col = "blue", main = title)
       }
     }
   }
@@ -36,14 +36,14 @@ plotts.wge = function (x, yLabel = "", Presentation = 0, title = "Realization", 
     if (class(x) != "ts") {
       df = data.frame(x = t, y = x)
       df %>% ggplot(aes(x = x, y = y)) + geom_point() + 
-        geom_line() + ggtitle(title) + xlab(xLabel) + 
-        ylab(yLabel)
+        geom_line() + ggtitle(title) + xlab(xlab) + 
+        ylab(ylab)
     }
     else if (class(x) == "ts") {
       df = data.frame(x = zoo::as.yearmon(time(x)), y = x)
       df %>% ggplot(aes(x = x, y = y)) + geom_point() + 
-        geom_line() + ggtitle(title) + xlab(xLabel) + 
-        ylab(yLabel)
+        geom_line() + ggtitle(title) + xlab(xlab) + 
+        ylab(ylab)
     }
   }
 }
