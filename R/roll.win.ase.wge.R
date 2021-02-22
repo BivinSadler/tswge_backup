dc5eac6dcbf634e36033a5f73ca36f9e7a4cc1e0
Sadler, Bivin Philip <bsadler@mail.smu.edu>
2021-02-22 16:19:40 -0600

    sdfsdf

--- R/roll.win.ase.wge.R
+++ R/roll.win.ase.wge.R
@@ -1,22 +1,24 @@
 #Rolling Window ASE Function
 # series is the array of the series
 # horizon is how far you want to predict into the future
-# training size should be at least max(p,q,s,d)
-# horizon is how far you want to predict into the future
 # d is the order of the differencing: (1-B^)^d
 # s is the order of the seasonality: (1-B^s)
 # phis = order of the stationary AR term
 # thetas = order of the invertible MA term
 
-
-
-roll.win.ase.wge = function(series, trainingSize, horizon = 1, s = 0, d = 0, phis = 0, thetas = 0)
+roll.win.ase.wge = function(series, horizon = 1, s = 0, d = 0, phis = 0, thetas = 0)
 {
   
-  for( i in 1:(length(series)-(trainingSize + horizon) + 1))
+  trainingSize = max(length(phis),length(thetas),s,d) + 1
+  numwindows = length(series)-(trainingSize + horizon) + 1
+  ASEHolder = numeric(numwindows)
+
+  print(paste("Please Hold For a Moment, TSWGE is processing the Rolling Window ASE with", numwindows, "windows."))
+  
+  for( i in 1:numwindows)
   {
     
-    forecasts = fore.aruma.wge(series[i:(i+(trainingSize-1))],phi = phis, theta = thetas, s = s, d = d,n.ahead = horizon)
+    invisible(capture.output(forecasts <- fore.aruma.wge(series[i:(i+(trainingSize-1))],phi = phis, theta = thetas, s = s, d = d,n.ahead = horizon)))
     
     ASE = mean((series[(trainingSize+i):(trainingSize+ i + (horizon) - 1)] - forecasts$f)^2)
     
@@ -25,11 +27,11 @@ roll.win.ase.wge = function(series, trainingSize, horizon = 1, s = 0, d = 0, phi
   }
   
   ASEHolder
-  hist(ASEHolder)
+  hist(ASEHolder, main = "ASEs for Individual Windows")
   WindowedASE = mean(ASEHolder)
   
   print("The Summary Statistics for the Rolling Window ASE Are:")
   print(summary(ASEHolder))
-  print(paste("The Rolling Window ASE is: ",WindowedASE))
-  return(WindowedASE)
+  print(paste("The Rolling Window ASE is: ",round(WindowedASE,3)))
+  return(list(rwASE = WindowedASE, numwindows = numwindows, horizon = horizon, s = s, d = d, phis = phis, thetas = thetas))
 }
