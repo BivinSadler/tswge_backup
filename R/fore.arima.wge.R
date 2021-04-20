807e5eb1ddd71d5e45ebec359091e38f167a8afd
Sadler, Bivin Philip <bsadler@mail.smu.edu>
2021-04-20 16:02:09 -0500

    sdfsdf

--- R/fore.arima.wge.R
+++ R/fore.arima.wge.R
@@ -1,9 +1,16 @@
+#
 fore.arima.wge=function(x,phi=0,theta=0,d=0,s=0, n.ahead=5,lastn=FALSE, plot=TRUE,alpha=.05,limits=TRUE)
 {
 n=length(x)
 p=length(phi)
-if(phi==0) {p=0
+phitot.res=0
+ptot.fore=0
+if(sum(phi^2)==0) {p=0
             fac1=0}
+#else {fac1=phi}
+
+#if(phi==0) {p=0
+#            fac1=0}
 if(sum(phi^2)>0) {fac1=phi}
 q=length(theta)
 if(sum(theta^2)==0) {q=0}
@@ -18,32 +25,34 @@ if (d==2) {diffac=c(2,-1)}
 if (d==3) {diffac=c(3,-3,1)}
 if (d<4) {fac2=diffac} 
 cat('s=',s,'\n') 
-if(s==0) {fac3=0}
+if(s==0) {fac3=0
+seas=0}
 if(s > 0) {seas=rep(0,s)
            seas[s]=1
            fac3=seas}
-#cat('seas',seas,'\n')
-dlam=length(lambda)
+cat('seas',seas,'\n')
+#dlam=length(lambda)
 #cat('lambda',lambda,'\n')
-if(lambda==0) {dlam=0
-            fac4=0}
-if(sum(lambda^2)>0) {fac4=lambda}
+#if(lambda==0) {dlam=0
+#            fac4=0}
+#if(sum(lambda^2)>0) {fac4=lambda}
 #cat('lambda',lambda,'\n')
-prod.fore.tmp=mult.wge(fac1,fac2,fac3,fac4)
-prod.res.tmp=mult.wge(fac2,fac3,fac4)
+ptot.fore=p+d+s
+ptot.res=d+s
+prod.fore.tmp=mult.wge(fac1,fac2,fac3)
+prod.res.tmp=mult.wge(fac2,fac3)
 phitot.fore=prod.fore.tmp$model.coef
-phitot.res=prod.res.tmp$model.coef
-#cat('fac1',fac1,'\n')
-#cat('fac2',fac2,'\n')
-#cat('fac3',fac3,'\n')
-#cat('fac4',fac4,'\n')
-ptot.fore=p+d+s+dlam
-ptot.res=d+s+dlam
-cat('ptot.res=',ptot.res,'\n')
-cat('phitot.res',phitot.res,'\n')
-cat('ptot.fore=',ptot.fore,'\n')
-cat('phitot.fore',phitot.fore,'\n')
-y.arma=artrans.wge(x,phi.tr=phitot.res)
+if(ptot.res>0) phitot.res=prod.res.tmp$model.coef
+cat('fac1',fac1,'\n')
+cat('fac2',fac2,'\n')
+cat('fac3',fac3,'\n')
+
+cat(d,s,phitot.res,'\n')
+  cat('ptot.res=',ptot.res,'\n')
+ cat('phitot.res',phitot.res,'\n')
+ cat('ptot.fore=',ptot.fore,'\n')
+ cat('phitot.fore=',phitot.fore,'\n')                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+y.arma=artrans.wge(x,phi.tr=phitot.res,plottr=FALSE)
 #
 
 #BACKCAST RESIDUALS BASED ON FIT TO TRANSFORMED ARMA DATA
@@ -138,27 +147,29 @@ maxp=max(x,ulplot)}
 #numcols <- 1
 timelab <- 'Time'
 valuelab <- ''
-fig.width <- 5
-fig.height <- 2.5
-cex.labs <- c(1.2,1.2,1.2) #eight,units='in',compression='none',res=350)
+#fig.width <- 5
+#fig.height <- 2.5
+cex.labs <- c(1.2,1.2,1.2)
+#tifffilename=filename,width=fig.width,height=fig.height,units='in',compression='none',res=350)
 #par(mfrow=c(numrows,numcols),mar=c(9,4,3,2))
 t<-1:n;
 np1=n+1
 np.ahead=mm+n.ahead
 tf<-mm:np.ahead
 if (plot==TRUE) {
-fig.width <- 5
-fig.height <- 2.5
+#fig.width <- 5
+#fig.height <- 2.5
 cex.labs <- c(1.2,1.2,1.2)
 #tiff(filename=filename,width=fig.width,height=fig.height,units='in',compression='none',res=350)
-par(mfrow=c(numrows,numcols),mar=c(9,4,3,2))
-plot(t,x,type='o',xaxt='n',yaxt='n',cex=.8,pch=16,cex.lab=1,cex.axis=1,lwd=1,xlab='',ylab='',xlim=c(1,maxh),ylim=c(minp,maxp),col=1)
+#par(mfrow=c(numrows,numcols),mar=c(9,4,3,2))
+if(n>=200) plot(t,x,type='l',xaxt='n',yaxt='n',cex=.8,pch=16,cex.lab=1,cex.axis=1,lwd=1,xlab='',ylab='',xlim=c(1,maxh),ylim=c(minp,maxp),col=1)
+if(n<200) plot(t,x,type='o',xaxt='n',yaxt='n',cex=.8,pch=16,cex.lab=1,cex.axis=1,lwd=1,xlab='',ylab='',xlim=c(1,maxh),ylim=c(minp,maxp),col=1)
 axis(side=1,cex.axis=1.1,mgp=c(3,0.15,0),tcl=-.3);
 axis(side=2,las=1,cex.axis=1.1,mgp=c(3,.4,0),tcl=-.3)
 mtext(side=c(1,2,1),cex=cex.labs,text=c(timelab,valuelab,""),line=c(1.2,2.1,1.8))
 points(tf,fplot,type='o',lty=1,cex=.6,lwd=1,pch=1,col=2);
-if(limits==TRUE) {points(tf,ulplot,type='l',lty=3,cex=0.6,lwd=.75,pch=1,col=4)
-points(tf,llplot,type='l',lty=3,cex=0.6,lwd=.75,pch=1,col=4)}
+if(limits==TRUE) {points(tf,ulplot,type='l',lty=3,cex=0.6,lwd=2,pch=1,col='blue3')
+points(tf,llplot,type='l',lty=3,cex=0.6,lwd=2,pch=1.3,col='blue3')}
     }
 np1=mm+1
 nap1=n.ahead+1
