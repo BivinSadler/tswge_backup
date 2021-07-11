5b5075da798f2ea4abbd4a26eb7cdbc81934f62b
Sadler, Bivin Philip <bsadler@mail.smu.edu>
2021-07-10 23:57:13 -0500

    sdfsdf

--- R/unit.circle.wge.R
+++ R/unit.circle.wge.R
@@ -11,15 +11,16 @@ unit.circle.wge = function(real = 0, imaginary = 0)
 {
 
 #Coordiate Plane and Circle
+
+#im = mtext(expression(paste(italic("im"))), side = 2, line = -19.9, at = 1.3)
+#re = mtext(expression(paste(italic("re"))), side = 3, line = -14.5, at = 1.4)
+  
 plot.new()
-plot(-1.5:1.5, -1.5:1.5,type="n",asp = 1, xlab="",ylab="",main="Roots and the Unit Circle")
+plot(-1.5:1.5, -1.5:1.5,type="n",asp = 1, xlab=expression(paste(italic("re"))),ylab=expression(paste(italic("im"))),main="Roots and the Unit Circle")
 
 draw.circle(0,0,1,border="blue",lty=2,lwd=2)
 abline(h = 0)
 abline(v = 0)
-
-mtext("imaginary", side = 2, line = -19.9, at = 1.3)
-mtext("real", side = 3, line = -14.5, at = 1.4)
 
 # plot the point designated by (real,imaginary)
 points(real,imaginary, pch = 3)
@@ -29,11 +30,11 @@ lines(c(0,real), c(0,imaginary), lwd = 3, col = "red")
 length = sqrt(real^2 + imaginary^2)
 
 #Display distance from origin and Abs Reciprocal Labels
-mtext("Length", side = 3, at = c(-1.8), line = -1)
-mtext("Abs Reciprocal", side = 3, at = c(1.5), line = -1)
+#mtext("Length", side = 3, at = c(-1.8), line = -1)
+#mtext("Abs Reciprocal", side = 3, at = c(1.5), line = -1)
 
 #Display distance from origin and Abs Reciprocal Values
-mtext(as.character(round(length,4)), side = 3, at = c(-1.8), line = -1.9)
-mtext(as.character(round(1/abs(length),4)), side = 3, at = c(1.5), line = -1.9)
+#mtext(as.character(round(length,4)), side = 3, at = c(-1.8), line = -1.9)
+#mtext(as.character(round(1/abs(length),4)), side = 3, at = c(1.5), line = -1.9)
 
 }
