707e574f5ff4f9dfaf4fd9ba91dae40fc8698dc0
Sadler, Bivin Philip <bsadler@mail.smu.edu>
2021-07-22 10:58:19 -0700

    jhg

--- R/pi.weights.wge.R
+++ R/pi.weights.wge.R
@@ -2,5 +2,5 @@ pi.weights.wge=function(phi=0,theta=0,lag.max=5)
 {
 ar=phi
 ma=-theta
-piwt=ARMAtoAR(ar,ma,lag.max)
+piwt=stats::ARMAtoAR(ar,ma,lag.max)
 return(piwt)}
--- R/roll.win.rmse.nn.wge.R
+++ R/roll.win.rmse.nn.wge.R
@@ -11,12 +11,21 @@
 # overwrite the instantiated mlp object's difforder (in the loop) with the old model difforder.
 
 
-roll.win.rmse.nn.wge = function(series, horizon = 1, s = 0, d = 0, fit_model)
+roll.win.rmse.nn.wge = function(series, horizon = 1, fit_model)
 {
-  
+
+if(fit_model$sdummy == FALSE)
+{
   lags = fit_model$lags
-  trainingSize = max(lags,s,d,9) #Long story but forecast::ets does not like series smaller than 8 unit and nnfor calls "trendcheck" when difforder = 0 which returns error for lenth < 8.
+  if(is_null(fit_model$difforder)){d = 0}
+  else{
+  d = max(fit_model$difforder)
+  }
+  s = frequency(series)
+  trainingSize = max(lags+sum(d)+2,9) #Long story but forecast::ets does not like series smaller than 8 unit and nnfor calls "trendcheck" when difforder = 0 which returns error for lenth < 8.
+  print(trainingSize)
   numwindows = length(series)-(trainingSize + horizon) + 1
+  print(numwindows)
   RMSEHolder = numeric(numwindows)
   MSEHolder = numeric(numwindows)
   #ForecastHolder = numeric(numwindows)
@@ -43,4 +52,47 @@ roll.win.rmse.nn.wge = function(series, horizon = 1, s = 0, d = 0, fit_model)
   print(summary(RMSEHolder))
   print(paste("The Rolling Window RMSE is: ",round(WindowedRMSE,3)))
   invisible(list(rwRMSE = WindowedRMSE, rwMSE = WindowedMSE, numwindows = numwindows, horizon = horizon, s = s, d = d))
-}
+} # end big if
+else   # This is if there is seasonality ... the problem is that if you give the model, mlp() still recalculates the seasonal dummies.
+{
+    lags = fit_model$lags
+    if(is_null(fit_model$difforder)){d = 0}
+    else{
+      d = max(fit_model$difforder)
+    }
+    s = frequency(series)
+    trainingSize = 20 # It will recalculate seasonal dummies... need large portion of data
+    print(trainingSize)
+    #numwindows = length(series)-(trainingSize + horizon) + 1
+    numwindows = 1
+    print(numwindows)
+    RMSEHolder = numeric(numwindows)
+    MSEHolder = numeric(numwindows)
+    #ForecastHolder = numeric(numwindows)
+    
+    print(paste("Please Hold For a Moment, TSWGE is processing the Rolling Window RMSE with", numwindows, "windows."))
+    
+    for( i in 1:numwindows)
+    {
+      fit = mlp(ts(series[1:(length(series)-horizon)], frequency = s), model = fit_model)
+      fit$minmax = fit_model$minmax
+      fit$difforder = fit_model$difforder
+      #print(fit)
+      #print(fit$ff.det)
+      forecasts = predict(fit,h = horizon)$mean
+      MSE = mean((series[(length(series)-(horizon)+1):length(series)] - forecasts)^2)
+      MSEHolder[i] = MSE
+      #ForecastHolder[i] = forecasts
+    }
+    
+    RMSEHolder = sqrt(MSEHolder)
+    hist(RMSEHolder, main = "RMSEs for Individual Windows")
+    WindowedMSE = mean(MSEHolder)
+    WindowedRMSE = mean(RMSEHolder)
+    
+    print("The Summary Statistics for the Rolling Window RMSE Are:")
+    print(summary(RMSEHolder))
+    print(paste("The Rolling Window RMSE is: ",round(WindowedRMSE,3)))
+    invisible(list(rwRMSE = WindowedRMSE, rwMSE = WindowedMSE, numwindows = numwindows, horizon = horizon, s = s, d = d, MH= MSEHolder))
+  } # end big else
+} # end function 
